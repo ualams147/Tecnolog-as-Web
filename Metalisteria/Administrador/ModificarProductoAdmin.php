ae27028d20f2250f15b85eb75cab0371f4ae2517
Ana Ruiz <arg062@inlumine.ual.es>
2025-12-13 13:47:27 +0100

    styles ModificarProducto

--- Metalisteria/Administrador/ModificarProductoAdmin.php
+++ Metalisteria/Administrador/ModificarProductoAdmin.php
@@ -68,7 +68,7 @@ if (!$producto) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Modificar Producto - Metalistería Fulsan</title>
-    <link rel="stylesheet" href=".../css/stylesModificarProductosAdmin.css">
+    <link rel="stylesheet" href="../css/stylesModificarProductosAdmin.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
