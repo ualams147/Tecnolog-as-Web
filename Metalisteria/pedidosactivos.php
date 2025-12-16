630a1fbf885774ab98061f6d38b0f3c6dee60204
MARIA LUISA SÁNCHEZ JIMÉNEZ <msj557@inlumine.ual.es>
2025-12-16 22:54:09 +0100

    para que funcione la puta página

--- Metalisteria/pedidosactivos.php
+++ Metalisteria/pedidosactivos.php
@@ -100,7 +100,7 @@ try {
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&family=Source+Sans+Pro:wght@700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     
-    <link rel="stylesheet" href="css/pedidosActivos.css">
+    <link rel="stylesheet" href="css/pedidosactivos.css">
     
 </head>
 
